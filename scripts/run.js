8f9068226cd78f92b4e102ca6be0d3cca7e809d4
sajicode <sajioloye@gmail.com>
2021-09-11 19:19:53 +0100

    chore: update functions

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,22 @@
 async function main() {
+  const [owner, randomPerson] = await ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+
+  waveCount = await waveContract.getTotalWaves();
 }
 
 main()
