8e9c5d60052f21b9ec035729457fc837a5c32e08
sajicode <sajioloye@gmail.com>
2021-09-11 18:52:13 +0100

    chore: init app and set up "Wave" portal

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import  "hardhat/console.sol";
+
+contract WavePortal {
+  uint totalWaves;
+  constructor() {
+    console.log("Yo, A contract is begeth");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s is waved", msg.sender);
+  }
+
+  function getTotalWaves() view public returns (uint)  {
+    console.log("We have %d total waves", totalWaves);
+    return totalWaves;
+  }
+}
